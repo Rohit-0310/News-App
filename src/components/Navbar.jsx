cb3cfb3f17bcd2617f603fa6672a29d3a9dbe664
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-08-16 10:12:28 +0530

    news app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./NavStyle.scss";
+
+
+
+const Navbar = () => {
+  return (
+    <div className='topNav'>
+        <div>Home Page</div>
+        <div>Dashboard</div>
+        <div>Sports News</div>
+        <div>About</div>
+    </div>
+  )
+}
+
+export default Navbar
